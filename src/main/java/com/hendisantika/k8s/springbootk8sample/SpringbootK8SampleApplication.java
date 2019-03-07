f7c5d6db4e7e8bcb290cd5aea85bd8df0f7ba02f
hendisantika <hendisantika@yahoo.co.id>
2019-03-07 07:43:00 +0700

    #1 - Initial Commit

---
+++ src/main/java/com/hendisantika/k8s/springbootk8sample/SpringbootK8SampleApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.k8s.springbootk8sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootK8SampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringbootK8SampleApplication.class, args);
+    }
+
+}
